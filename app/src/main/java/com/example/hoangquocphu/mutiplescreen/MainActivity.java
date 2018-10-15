71a57c0fe21326dec700878b267d12bcf5bf5695
QuocPhuDEV <phuhq.it@gmail.com>
2018-10-15 13:36:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hoangquocphu/mutiplescreen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hoangquocphu.mutiplescreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
